8d78b8125a0bc9aad77951a96b5aa30681448eee
drummerevans <drummerevans@live.co.uk>
2018-11-17 20:56:36 +0000

    PHY2026 Directory

---
+++ PHY2026/LS_Poly.py
@@ -0,0 +1,44 @@
+import numpy as np
+# from scipy.interpolate import *
+import matplotlib.pyplot as plt
+import matplotlib.ticker
+import statistics
+
+def least_squares_fit(x, y, err_x, err_y):
+    plt.rc('font', family = 'serif', serif = 'cmr10') 
+    plt.rcParams["axes.linewidth"] = 1.0 
+
+    arr_size = int(len(x))
+    N = arr_size - 1 # N = x(arr_size - 1) # where N is the final element in the array (of x vals)
+                        # we have to -1 as elements in array start count at 0 NOT 1
+
+    p = np.polyfit(x, y, 1) # finds the coefficients for the 'best' fitting function
+  
+    f = np.polyval(p, x) # these are the 'y-values' of the fitting function
+    sigma = statistics.stdev(f - y) # standard deviation of quantity 'f - y'
+
+    # plt.figure(figsize = (8, 6))
+    plt.errorbar(x, y, err_x, err_y, fmt = "k+", capsize = 2, elinewidth = 0.6, MarkerSize = 2, markeredgewidth = 0.6, LineStyle = "none")
+    # the last argument for the two lines below, is for the legend
+    # plt.plot(x, y, Marker = "+", MarkerSize = 4, MarkerEdgeColor = "k", MarkerFaceColor = "k", LineWidth = 0.8, LineStyle = "none")
+    plt.plot(x, f, LineWidth = 0.6, Linestyle = "-", Color = "b", label = "Model") # 'line of best fit'
+
+    # plt.title("Least Squares Fit", fontsize = 12, fontweight = "bold")
+    plt.xlabel('$\\sqrt{d^2 + \\ell^2}$/mm', fontsize = 10)
+    plt.ylabel("$\\ell$/mm", fontsize = 10)
+    # plt.axis([150, 350, 150, 350])
+    # plt.xlim(-1, 6)
+    # ax.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1))
+    # plt.ylim(-2, 2)
+    # ax.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1))
+    plt.gca().tick_params(width = 1.0, labelsize = 8)
+  
+    plt.savefig("Blue_Plot.pdf")
+
+    print("Gradient is: {:f}" .format(p[0]))
+    print("y-intercept is: {:f}" .format(p[1]))
+    print("Sigma is: {:f}" .format(sigma))
+    
+    # The next two lines are for linear i.e. straight line fits only
+    grad_err = (2 * sigma) / (x[N] - x[0])
+    print("Gradient error is: {:f}" .format(grad_err))
---
+++ PHY2026/least_squares.py
@@ -0,0 +1,40 @@
+import numpy as np
+# from scipy.interpolate import *
+import matplotlib.pyplot as plt
+import matplotlib.ticker
+import statistics
+from LS_Poly import least_squares_fit
+
+x_vals = []
+y_vals = []
+err_horiz = [] 
+err_vert = [] 
+
+fptr = open("blue_data.txt", "r", newline=None)
+
+list_of_results = fptr.readlines()
+
+data  = []
+
+for result in list_of_results:
+    datum = result.split( )
+    for i in range(0, len(datum)):
+        if i == 0:
+            x_vals.append(float(datum[i]))
+        elif i == 1:
+            y_vals.append(float(datum[i]))
+        elif i == 2:
+            err_horiz.append(float(datum[i]))
+        else:
+            err_vert.append(float(datum[i]))
+
+fptr.close()
+
+print(x_vals)
+
+# x_vals = np.array([0, 1, 2, 3, 4, 5])
+# y_vals = np.array([0, 0.8, 0.9, 0.1, -0.8, -1])
+# err_horiz = np.array([0.2, 0.1, 0.5, 0.3, 0.1, 0.2]) 
+# err_vert = np.array([0.2, 0.1, 0.5, 0.3, 0.1, 0.2]) 
+
+least_squares_fit(x_vals, y_vals, err_horiz, err_vert) # calling the function to plot the graph
